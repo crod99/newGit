f4cc8fca36488028cac365817d6edbcf3bcb1fbb
crod99 <crodriguez9992@students.mc3.edu>
2022-11-11 18:19:01 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World");
+      
+      int n = 100;
+      for(int i = 1; i <= n; i++)
+      {
+         System.out.println(i);
+      }
+   }
+}
