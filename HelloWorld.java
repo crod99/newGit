c4a12e99f1c8435d80746985b981b5cd8a4b8584
crod99 <crodriguez9992@students.mc3.edu>
2022-11-11 18:30:23 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,3 +11,12 @@ public class HelloWorld
       }
    }
 }
+/*Reflection
+Using Github is really confusing. First, I couldn't quite understand
+how to create the repository. Created two folders and put my work in
+the wrong folder. When I checked the repository it wasn't showing.
+Then I was able to see it in the correct folder. Was able to push my
+commits to the remote repository on Github, I think that was the easiest.
+Being able to see it on the website went well, which I think I did everything
+how it is supposed to be. But yes, Github is not very user friendly.
+Did take some time to get the things done.*/
